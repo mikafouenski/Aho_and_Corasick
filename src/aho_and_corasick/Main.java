5a8f733899516b2b27d12c57cd1bd6664b49fa2e
Mickaël Bernardini <contact@mickael-bernardini.fr>
2015-09-24 15:38:57 +0200

    add text files

--- src/aho_and_corasick/Main.java
+++ src/aho_and_corasick/Main.java
@@ -1,10 +1,19 @@
 package aho_and_corasick;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-
+		BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.print(bufferedreader.readLine());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
