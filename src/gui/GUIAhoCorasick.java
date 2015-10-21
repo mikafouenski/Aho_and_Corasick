61af4a90a99185a3d979184d4a3e481f8b260676
tentrenche <tentrenche@gmail.com>
2015-10-21 19:56:19 +0200

    Revert "Revert "add gui""
    This reverts commit 50ccdb2e743475f9174ea997eedcba3320d9a319.

--- src/gui/GUIAhoCorasick.java
+++ src/gui/GUIAhoCorasick.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Writer;
 import java.nio.file.Files;
+import java.util.Iterator;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -28,6 +29,9 @@ import javax.swing.text.Highlighter;
 import javax.swing.text.Highlighter.HighlightPainter;
 
 import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import aho_and_corasick.AhoCorasick;
+import aho_and_corasick.SearchResult;
 
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
@@ -53,6 +57,7 @@ public class GUIAhoCorasick extends JFrame {
 	private JTextField textLookFor;
 	private JTextArea textFound;
 	private static JTextArea jtext;
+	private AhoCorasick tree = new AhoCorasick();
 
 	/**
 	 * Launch the application.
@@ -223,15 +228,23 @@ public class GUIAhoCorasick extends JFrame {
 
 	private void Add(){
 		String word = textLookFor.getText();
-		aho_and_corasick.Main.tree.add(word.getBytes(), word);
+		tree.add(word.getBytes(), word);
 
 		textFound.append(word + "\n\n");
 		textLookFor.setText(null);
 	}
 
 	private void Search(){
-		aho_and_corasick.Main.text = jtext.getText();
-		aho_and_corasick.Main.main(null);
+		tree.prepare();
+		String text = jtext.getText();
+
+		Iterator searcher = tree.search(text.getBytes());
+		while (searcher.hasNext()) {
+			SearchResult result = (SearchResult) searcher.next();
+			System.out.println(result.getDisplay());
+			System.out.println("Found at index: " + result.getLastIndex());
+			Result(result.getDisplay().toString(), result.getLastIndex());
+		}
 	}
 
 	public static void Result(String result, int index){
@@ -246,8 +259,6 @@ public class GUIAhoCorasick extends JFrame {
 		} catch (BadLocationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-
-		
+		}		
 	}
 }
